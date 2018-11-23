639104ba3e69b55c5b858d8f792adf3ae25bb75b
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-11-23 21:51:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.login(process.env.CB);
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.username} \n Prefix ${prefix}`)
 client.user.setStatus('online');
-client.user.setActivity(`Critical Network`, { type : "LISTENING" })
+client.user.setActivity(`» C'R 100 user`, { type : "LISTENING" })
 });
 
 
@@ -46,6 +46,7 @@ client.on("message", message => {
     if(reason.length < 1) return message.reply(`Set a reason`);
     if(!reason.includes(`prntscr.com`)) return message.reply(`Reason must fe **https://prntscr.com** Screen shot!`);
     if(!message.member.roles.find("name", "Support Team")) return message.reply(`You must have **Support Team** Role`);
+    if(user.roles.find('name', "Muted")) return message.reply(`**:x: | The user is muted before!**`)
     var embed = new Discord.RichEmbed();
     embed.setAuthor(`${user.user.username}`, user.avatarURL)
     embed.setThumbnail(user.avatarURL)
@@ -60,6 +61,32 @@ client.on("message", message => {
     message.channel.send(`**Reason saved and will review by our staff**`);
       message.delete();
     user.addRole(message.guild.roles.find('name', 'Muted'));
+    log.send({embed})
+  }
+});
+
+
+client.on("message", message => {
+  var user = message.mentions.members.first();
+  var log = message.guild.channels.find("name", "log");
+  if(message.content.startsWith(`${prefix}unmute`)){
+    if(!user) return message.reply(`Mention someone`);   if(!log) return message.reply(`Create text channel named #log`);
+    if(!message.member.roles.find("name", "Support Team")) return message.reply(`You must have **Support Team** Role`);
+    if(!user.roles.find('name', "Muted")) return message.reply(`**:x: | The user is not muted!**`)
+    var embed = new Discord.RichEmbed();
+    embed.setAuthor(`${user.user.username}`, user.avatarURL)
+    embed.setThumbnail(user.avatarURL)
+    embed.setTitle(`New Unmute!`)
+    embed.addField(`For`, `<@${user.id}>`)
+    embed.addField(`By`, `<@${message.author.id}>`)
+    embed.addField(`In Chat`, `<#${message.channel.id}>`)
+    embed.addField(`Reason`, `${reason}`)
+    embed.setTimestamp()
+    embed.setColor("RED")
+    embed.setFooter(" ")
+    message.reply(`**Done!**`)
+      message.delete();
+    user.removeRole(message.guild.roles.find('name', 'Muted'));
     log.send({embed})
   }
 });
