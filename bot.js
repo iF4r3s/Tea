0bbd13aee97c4a45474833241f2c0e7f0d475ac9
iF4r3s <44950873+iF4r3s@users.noreply.github.com>
2018-11-27 21:39:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,10 +21,10 @@ client.on("message", message => {
 
         let args = message.content.split(" ").slice(1);
         var argresult = args.join(' ');
-        message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+        message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
             m.send(`${argresult}\n ${m}`);
         })
-        message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'all').size}\` : :twitter_pepe:عدد الاعضاء المستلمين`);
+        message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'offline').size}\` : :twitter_pepe:عدد الاعضاء المستلمين`);
         message.delete();
     }
 });
